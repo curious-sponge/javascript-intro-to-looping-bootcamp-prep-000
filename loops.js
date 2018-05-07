1e7cd73ff57e568f4a22362c53ce08b620e212ba
sedat_3278@hotmail.com <sedat_3278@hotmail.com>
2018-05-07 11:53:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(anArray){
     for (let i = 0; i < 25; i++){
-      anArray[i] = `"I am ${i} strange loop${i === 1 ? '' : 's'}."`
+      anArray.push( `"I am ${i} strange loop${i === 1 ? '' : 's'}."`)
     }
     return anArray;
   }
